ffc83cc03b00cfb1267d159912b5185de0be158b
Hélène de Fromont <h.defromont@gmail.com>
2019-05-13 21:07:32 +0200

    Update character-sheet.user.js

--- userscripts/character-sheet.user.js
+++ userscripts/character-sheet.user.js
@@ -20,162 +20,158 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+	'use strict';
 
-    var retrieveCharacterData = function(post) {
-        var profile = $(post).find(".post-author-name a").attr("href");
+	var retrieveCharacterData = function(post) {
+		var profile = $(post).find(".post-author-name a").attr("href");
 
-        var data = {
-            metiers: [],
-            profil: profile
-        };
+		var data = {
+			metiers: [],
+			profil: profile
+		};
 
-        var perso = $(post).find("perso").first();
-        $(perso).find(".data").each(function() {
-            var name = $(this).data("form");
-            var value = $(this).text();
+		var perso = $(post).find("character").first();
+		$(perso).find(".data").each(function() {
+			var name = $(this).data("form");
+			var value = $(this).text();
 
-            if(name === "metier") {
-                var metier = {
-                    id: $(this).data("metier"),
-                    value: value
-                };
-                data.metiers.push(metier);
-            } else if(name === "genre") {
-                var more = value;
-                if(value !== "Femme" && value !== "Homme") {
-                    value = "Autre";
-                }
-                data[name] = {value: value, more: more};
-            } else if(name === "type") {
-                data[name] = value;
-                data.lien = $(this).data("lien");
-            } else {
-                data[name] = value;
-            }
-        });
+			if(name === "metier") {
+				var metier = {
+					id: $(this).data("metier"),
+					value: value
+				};
+				data.metiers.push(metier);
+			} else if(name === "genre") {
+				var more = value;
+				if(value !== "Femme" && value !== "Homme") {
+					value = "Autre";
+				}
+				data[name] = {value: value, more: more};
+			} else if(name === "type") {
+				data[name] = value;
+				data.lien = $(this).data("lien");
+			} else {
+				data[name] = value;
+			}
+		});
 
-        console.log(data);
-        return data;
-    };
+		console.log(data);
+		return data;
+	};
 
-    var generateString = function() {
-        var post = $(this).parents(".post-wrap");
-        var data = retrieveCharacterData(post);
+	var generateString = function() {
+		var post = $(this).parents(".post-wrap");
+		var data = retrieveCharacterData(post);
 
-        var text = data.nom + "	" + data.prenom + "	" + data.genre.value + "	" + data.groupe + "	" + data.type + "	";
+		var text = data.nom + "	" + data.prenom + "	" + data.genre.value + "	" + data.groupe + "	" + data.type + "	";
 
-        if(data.type === "Scénario") {
-            text += "Pris";
-        } else {
-            text += "	";
-        }
+		if(data.type === "Scénario") {
+			text += "Pris";
+		} else {
+			text += "	";
+		}
 
-        text += "	" + data.profil + "	" + data.lien;
+		text += "	" + data.profil + "	" + data.lien;
 
-        data.metiers.forEach(function(element) {
-            text += "	" + element.id;
-        });
+		data.metiers.forEach(function(element) {
+			text += "	" + element.id;
+		});
 
-        //text.select();
-        //document.execCommand("copy");
+		//text.select();
+		//document.execCommand("copy");
 
-        GM_setClipboard(text, "text");
-        console.log(text);
-    };
+		GM_setClipboard(text, "text");
+		console.log(text);
+	};
 
-    var countWords = function() {
-        $("block[data-form]").each(function() {
-            var count = $(this).text().replace(/\[.*?\]/g," ").replace(/<.*?>/g," ").replace(/[\x00-\x40\x5b-\x60\x7b-\x7e]/g," ").split(" ").length;
-            var wordcount = "<div class='wordcount' data-count='" + count + "'><i class='material-icons'></i>" + count + " mots</div>";
-            $(this).append(wordcount);
+	var countWords = function() {
+		$("block[data-form]").each(function() {
+			var count = $(this).text().replace(/\[.*?\]/g," ").replace(/<.*?>/g," ").replace(/[\x00-\x40\x5b-\x60\x7b-\x7e]/g," ").split(" ").length;
+			var wordcount = "<div class='wordcount' data-count='" + count + "'><i class='material-icons'></i>" + count + " mots</div>";
+			$(this).append(wordcount);
 
-            switch($(this).data("form")) {
-                case "caractere":
-                case "physique":
-                    if(count >= 300) {
-                        $(this).find(".wordcount").addClass("valid");
-                    }
-                    break;
-                case "histoire":
-                    if(count >= 500) {
-                        $(this).find(".wordcount").addClass("valid");
-                    }
-                    break;
-            }
-        });
-    };
+			switch($(this).data("form")) {
+				case "caractere":
+				case "physique":
+					if(count >= 300) {
+						$(this).find(".wordcount").addClass("valid");
+					}
+					break;
+				case "histoire":
+					if(count >= 500) {
+						$(this).find(".wordcount").addClass("valid");
+					}
+					break;
+			}
+		});
+	};
 
-    var nbReason = 0;
+	var nbReason = 0;
 
-    var addReason = function() {
-    	nbReason += 1;
+	var addReason = function() {
+		nbReason += 1;
 
-    	var reason = '<li class="form-group"><textarea class="form-control" rows="3" name="other-' + nbReason + '" id="other-' + nbReason + '" placeholder="Raison n°' + nbReason + '"></textarea></li>';
+		var reason = '<li class="form-group"><textarea class="form-control" rows="3" name="other-' + nbReason + '" id="other-' + nbReason + '" placeholder="Raison n°' + nbReason + '"></textarea></li>';
 
-    	$("#rejectReasons").find("ol").append(reason);
-    }
+		$("#rejectReasons").find("ol").append(reason);
+	}
 
-    var removeReason = function() {
-    	$("#rejectReasons").find("li").last().detach();
-    	nbReason -= 1;
-    }
+	var removeReason = function() {
+		$("#rejectReasons").find("li").last().detach();
+		nbReason -= 1;
+	}
 
-    if($(".character").length > 0) {
-        var firstPost = $(".post").has(".character");
-        var username = $(firstPost).find(".post-author-name").text();
-        var groupId = $(firstPost).find(".data[data-form='groupe']").data("group");
+	if($(".character").length > 0) {
+		var firstPost = $(".post").has(".character");
+		var username = $(firstPost).find(".post-author-name").text();
+		var groupId = $(firstPost).find(".data[data-form='groupe']").data("group");
 
-        var modButtons = GM_getResourceText("header");
-        $(firstPost).find(".post-body").prepend(modButtons);
-	    
+		var modButtons = GM_getResourceText("header");
+		$(firstPost).find(".post-body").prepend(modButtons);
+		
 		var customStyle = GM_getResourceText("style");
 		GM_addStyle(customStyle);
 
-        $("#listing").click(generateString);
-        $("#validate").click(function() {
-			console.log("validate");
-			
-            addToGroup(username, FA.Group[groupId]);
-            moveTopic(FA.Post.getTopicInfos(), FA.Group[groupId].forum);
+		$("#listing").click(generateString);
 
-            var message = GM_getResourceText("message-validated").replace("{USERNAME}", username);
+		// Fiche Correcte à Valider
+		$("#validate").click(function() {
+			addToGroup(username, FA.Group[groupId]);
+			moveTopic(FA.Post.getTopicInfos(), FA.Group[groupId].forum);
+
+			var message = GM_getResourceText("message-validated").replace("{USERNAME}", username).replace("{MESSAGE}", message);
 			FA.Post.postReply(FA.Post.getTopicInfos(), message);
-        });
+		});
 
-        // Message de Bienvenue
-        $("#welcome").click(function() {
-			console.log("welcome");
-			
-            var message = GM_getResourceText("message-welcome").replace("{USERNAME}", username);
-            FA.Post.postReply(FA.Post.getTopicInfos(), message);
-        });
-
-        // Message pour demander des Nouvelles
-        $("#news").click(function() {
-			console.log("news");
-			
-        	var message = GM_getResourceText("message-inactive").replace("{USERNAME}", username);
+		// Message de Bienvenue
+		$("#welcome").click(function() {
+			var message = GM_getResourceText("message-welcome").replace("{USERNAME}", username).replace("{MESSAGE}", message);
 			FA.Post.postReply(FA.Post.getTopicInfos(), message);
-        });
+		});
 
-        // Message pour archiver
-        $("#archive").click(function() {
+		// Message pour demander des Nouvelles
+		$("#news").click(function() {
+			var message = GM_getResourceText("message-inactive").replace("{USERNAME}", username);
+			FA.Post.postReply(FA.Post.getTopicInfos(), message);
+		});
+
+		// Message pour archiver
+		$("#archive").click(function() {
 			console.log("archive");
 			
-        	var message = GM_getResourceText("message-archive");
-            FA.Post.moveTopic(FA.Post.getTopicInfos(), FA.Archive.auberges.personnages);
-            FA.Post.postReply(FA.Post.getTopicInfos(), message);
-        });
+			var message = GM_getResourceText("message-archive");
+			FA.Post.moveTopic(FA.Post.getTopicInfos(), FA.Archive.auberges.personnages);
+			FA.Post.postReply(FA.Post.getTopicInfos(), message);
+		});
 
 		// Message pour demander des modifications
-        $("#reject").click(function() {
+		$("#reject").click(function() {
 			console.log("reject");
 			
-            var message = "";
-            FA.Post.postReply(FA.Post.getTopicInfos(), message);
-        });
+			var message = "";
+			FA.Post.postReply(FA.Post.getTopicInfos(), message);
+		});
 
-        countWords();
-    }
+		countWords();
+	}
 })();
